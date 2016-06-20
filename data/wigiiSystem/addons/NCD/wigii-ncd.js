57f62273036ec5b81464cb89c54d0c71e30a56fa
Camille Weber <cwebedev@users.noreply.github.com>
2016-06-20 18:04:20 +0200

    coded HtmlEmitter class

--- data/wigiiSystem/addons/NCD/wigii-ncd.js
+++ data/wigiiSystem/addons/NCD/wigii-ncd.js
@@ -35,11 +35,568 @@
 		/**
 		 * Object which holds inner private mutable state variables.
 		 */
-		wigiiNcd.context = {};
+		wigiiNcd.context = {};		
+						
+		// Error codes
+		
+		wigiiNcd.errorCodes = {
+				
+			// ServiceException (from 1000 to 1099 + http error codes)
+				
+			/**
+			 * unauthorized user, http equivalent
+			 */
+			UNAUTHORIZED: 401,
+			/**
+			 * access forbidden, http equivalent
+			 */
+			FORBIDDEN: 403,
+			/**
+			 * not found, http equivalent
+			 */
+			NOT_FOUND: 404,
+			/**
+			 * default error
+			 */
+			UNKNOWN_ERROR: 1000,
+			/**
+			 * development error, indicates that a method is still not implemented but will be in a short term
+			 */
+			NOT_IMPLEMENTED: 1001,
+			/**
+			 * indicates that nested (or previous) exception exists.
+			 */
+			WRAPPING: 1002,
+			/**
+			 * indicates that this operation is not supported by this implementation.
+			 */
+			UNSUPPORTED_OPERATION: 1003,
+			/**
+			 * indicates that a method argument is invalid
+			 */
+			INVALID_ARGUMENT: 1004,
+			/**
+			 * unexpected error, that should never happen.
+			 */
+			UNEXPECTED_ERROR: 1005,
+			/**
+			 * database is dirty with corrupted or invalid data
+			 */
+			DATA_INTEGRITY_ERROR: 1006,
+			/**
+			 * deprecated method
+			 */
+			DEPRECATED: 1007,
+			/**
+			 * invalid state of object
+			 */
+			INVALID_STATE: 1008,
+			/**
+			 * Indicates that there is a problem in the configuration of the system
+			 */
+			CONFIGURATION_ERROR: 1009,
+			/**
+			 * Indicates that the current operation has been explicitely canceled by the service
+			 * The service can provide a retryAfterCancel method.
+			 */
+			OPERATION_CANCELED: 1010,
+			
+			
+			// ListException (error code range from 3100 to 3199)
+			
+			OUT_OF_BOUND: 3100,
+			ALREADY_EXISTS: 3101,
+			DOES_NOT_EXIST: 3102,
+			
+			// StringTokenizerException (error code range from 3800 to 3899)			
+			SYNTAX_ERROR: 3800,
+			
+			// FuncExpEvalException (error code range from 4600 to 4699)
+			
+			SYMBOL_NOT_FOUND: 4600,
+			INVALID_RETURN_VALUE: 4601,
+			VARIABLE_NOT_DECLARED: 4602,
+			DIVISION_BY_ZERO: 4603,
+			ASSERTION_FAILED: 4604
+			
+		};
+		// initializes error labels
+		wigiiNcd.initializeErrorLabels = function() {
+			if(!wigiiNcd.errorLabels) {
+				wigiiNcd.errorLabels = {};
+				for(var errName in wigiiNcd.errorCodes) {
+					wigiiNcd.errorLabels[wigiiNcd.errorCodes[errName]] = errName;
+				}
+			}			
+		};
+		wigiiNcd.initializeErrorLabels();
+		
+		// NCD Exceptions
+		
+		/**
+		 * ServiceException class
+		 * @param String message the error message
+		 * @param Number code the error code
+		 * @param Object previous if defined, the previous exception in the chain if wrapping.
+		 */
+		wigiiNcd.ServiceException = function(message,code,previous) {			
+			var self = this;
+			self.name = 'ServiceException';
+			self.message = message;
+			self.code = code || wigiiNcd.errorCodes.UNKNOWN_ERROR;
+			self.previousException = previous; 
+		};
+		
+		// NCD Servies
+		
+		/**
+		 * A String Stack object
+		 */
+		wigiiNcd.StringStack = function() {
+			var self = this;
+			self.className = 'StringStack';
+			self.ctxKey = wigiiNcd.ctxKey+'_'+self.className;
+			
+			self.stack = [];
+			self.popSubscribers = [];
+			
+			/**
+			 * Pushes a new element on top of the stack
+			 *@return wigiiNcd.StringStack for chaining
+			 */
+			self.push = function(str) {
+				self.stack.push({start:str,content:'',end:undefined});
+				return self;
+			};
+			/**
+			 * Appends a string to current element in stack
+			 *@return wigiiNcd.StringStack for chaining
+			 */
+			self.append = function(str) {
+				var top = undefined;
+				if(self.stack.length>0) top = self.stack[self.stack.length-1];
+				if(!top) {
+					top = {start:'',content:''+str,end:undefined};
+					self.stack.push(top);
+				}
+				else top.content += str;
+				return self;
+			};
+			/**
+			 * Returns current content in stack
+			 *@return String current content
+			 */
+			self.current = function() {
+				if(self.stack.length>0) return self.stack[self.stack.length-1].content;
+				else return '';
+			};
+			/**
+			 * Resets stack
+			 *@return wigiiNcd.StringStack for chaining
+			 */
+			self.reset = function() {
+				self.stack = [];
+			};
+			/**
+			 * Pops top element from stack
+			 *@param String str optional closing element string to append to top element before poping
+			 *@return if stack is emptied then returns content, else returns wigiiNcd.StringStack for chaining
+			 */
+			self.pop = function(str) {
+				var top = self.stack.pop();
+				if(!top) top = {start:'',content:'',end:undefined};
+				top.end = ''+str;
+				// calls any registred eventHandlers
+				if(self.popSubscribers.length>0) {
+					for(var i=0;i<self.popSubscribers.length;i++) {
+						self.popSubscribers[i](top.start, top.content, top.end);
+					}
+				}
+				// if stack is not empty, then updates parent content
+				if(self.stack.length>0) {
+					self.stack[self.stack.length-1].content += top.start+top.content+top.end;
+				}
+				// else returns content
+				else return top.start+top.content+top.end;
+			};
+			/**
+			 * Registers an eventHandler which is called each time an element is poped
+			 *@param Function eventHandler a function with signature eventHandler(startTag, content, endTag)
+			 */
+			self.onPop = function(eventHandler) {
+				if(!$.isFunction(eventHandler)) throw wigiiNcd.createServiceException('pop event handler should be a function', wigiiNcd.errorCodes.INVALID_ARGUMENT);
+				self.popSubscribers.push(eventHandler);
+			};			
+		}
+		
+		/**
+		 * HTML Emitter object
+		 *@param jQuery|DOM.Element output the element in which to emit HTML code, defaults to body if not specified.
+		 */
+		wigiiNcd.HtmlEmitter = function(output) {
+			var self = this;
+			self.className = 'HtmlEmitter';
+			self.ctxKey = wigiiNcd.ctxKey+'_'+self.className;
+			self.options = {};
+			
+			if(!output) output = $('body');
+			else output = $(output);
+			
+			// Configuration
+			
+			self.emittedClass = function(cssClass) {
+				if(cssClass) self.options.emittedClass = cssClass;
+				if(!self.options.emittedClass) self.options.emittedClass = 'ncd';
+				return self.options.emittedClass;
+			}
+			
+			// HTML emitting functions
+			
+			/**
+			 * Outputs a string
+			 */
+			self.put = function(str){
+				self.htmlTree.append(str);
+			};
+			/**
+			 * Emits a <p> start tag
+			 */
+			self.startP = function(){
+				self.htmlTree.push(wigiiNcd.getHtmlBuilder().putStartTag('p','class',self.emittedClass()).html());
+			};
+			/**
+			 * Emits a </p> end tag
+			 */
+			self.endP = function(){
+				output.append(self.htmlTree.pop('</p>'));				
+			};
+			/**
+			 * Emits a <span> start tag
+			 */
+			self.startSpan = function(){
+				self.htmlTree.push(wigiiNcd.getHtmlBuilder().putStartTag('span','class',self.emittedClass()).html());
+			};
+			/**
+			 * Emits a </span> end tag
+			 */
+			self.endSpan = function(){
+				output.append(self.htmlTree.pop('</span>'));				
+			};
+						
+			// Control functions
+			
+			/**
+			 * Resets container
+			 */
+			self.reset = function() {
+				output.empty();
+			};
+			/**
+			 * Clears all errors from container
+			 */
+			self.clearErrors = function() {				
+				output.find('p.ncd-error').remove();
+			};
+			/**
+			 * Ends current HTML emitting session and controls stack
+			 */
+			self.end = function() {
+				self.htmlTree.pop('');
+			};
+			/**
+			 * Publishes any catched exception
+			 */
+			self.publishException = function(exception) {
+				htmlb = wigiiNcd.getHtmlBuilder();
+				htmlb.putStartTag('p','class',self.emittedClass()+'-error').put(exception.code).prepend(' ',wigiiNcd.errorLabels[exception.code]).putBr()
+				.implode(' : ',exception.name,exception.message.replace(/</g,'&lt;').replace(/>/g,'&gt;'))
+				.putEndTag('p');	
+				output.append(htmlb.html());
+			};
+			
+			// HTML tree check
+			
+			self.htmlTree = wigiiNcd.createStringStackInstance();
+			// checks that start and end tags are equal
+			self.htmlTree.onPop(function(start,content,end) {
+				if(!start && !end) return;
+				var i = end.indexOf('</');
+				var j = end.indexOf('>');
+				var endtag = end.substring(i+2,j).trim();
+				if(!endtag || start.indexOf(endtag)<0) throw wigiiNcd.createServiceException("invalid end tag '"+endtag+"' in context "+start+content.substr(0,64)+(content.length>64?'...':'')+end, wigiiNcd.errorCodes.SYNTAX_ERROR);
+			});
+		};
+		
+		/**
+		 * HTML String builder
+		 */
+		wigiiNcd.HtmlBuilder = function() {
+			var self = this;
+			self.className = 'HtmlBuilder';
+			self.ctxKey = wigiiNcd.ctxKey+'_'+self.className;
+			
+			self.buffer = '';
+			self.jsBuffer = [];
+			
+			/**
+			 * Returns built html string
+			 * @return String
+			 */
+			self.html = function() {
+				return self.buffer;
+			};
+			/**
+			 * Executes all the JS code stored into the buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.runJsCode = function() {
+				for(var i=0;i<self.jsBuffer.length;i++) {
+					var jsCode = self.jsBuffer[i];
+					if($.isFunction(jsCode)) jsCode();
+				}
+				return self;
+			};
+			/**
+			 * Resets the html builder to an empty buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.reset = function() {
+				self.buffer = '';
+				self.jsBuffer = [];
+				return self;
+			};
+			/**
+			 * Appends a string to current buffer
+			 * @param String str the string to put into the buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.put = function(str) {
+				if(str) self.buffer += str;
+				return self;
+			};
+			/**
+			 * Implodes some arguments with a separator and adds the string to the given buffer
+			 * @param String sep the separator to be added between each arguments
+			 * @return HtmlBuilder for chaining
+			 */
+			self.implode = function(sep) {
+				if(arguments.length>1) {
+					var i = 1;
+					var result = '', value;
+					while(i<arguments.length) {						
+						value = arguments[i];
+						if(value) {
+							if(result) result += sep;
+							result += value;
+						}
+						i++;
+					}
+					self.buffer += result;
+				}
+				return self;
+			};
+			/**
+			 * Prepends a prefix to some content only if content is not null.
+			 * @param String prefix the string prefix
+			 * @param String str the content to put into the buffer prefixed if not null
+			 * @return HtmlBuilder for chaining
+			 */
+			self.prepend = function(prefix,str) {
+				if(str) self.buffer += prefix+str;
+				return self;
+			};
+			/**
+			 * Repeats an nbsp entity several times
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putNbsp = function(multiplier) {
+				if(!multiplier) multiplier=1;
+				for(var i=0;i<multiplier;i++) {
+					self.buffer += '&nbsp;';
+				}
+				return self;
+			};
+			/**
+			 * Repeats an br tag several times
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putBr = function(multiplier) {
+				if(!multiplier) multiplier=1;
+				for(var i=0;i<multiplier;i++) {
+					self.buffer += '<br/>';
+				}
+				return self;
+			};
+			/**
+			 * Puts a double quote entity in the buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putQuot = function() {
+				self.buffer += '&quot;';
+				return self;
+			};
+			/**
+			 * Puts an Amp entity in the buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putAmp = function() {
+				self.buffer += '&amp;';
+				return self;
+			};
+			/**
+			 * Puts a Lt entity in the buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putLt = function() {
+				self.buffer += '&lt;';
+				return self;
+			};
+			/**
+			 * Puts a Gt entity in the buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putGt = function() {
+				self.buffer += '&gt;';
+				return self;
+			};
+			/**
+			 * Puts an Apos entity in the buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putApos = function() {
+				self.buffer += '&apos;';
+				return self;
+			};
+			/**
+			 * Puts an HashTag character in the buffer
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putHashTag = function() {
+				self.buffer += '#';
+				return self;
+			};
+			/**
+			 * Creates an html open tag
+			 * @param String tagName the name of the html tag, for example "div" or "p"
+			 * @param String key an html attribute name, for example "class"
+			 * @param String value an html attribute value, for example "ui-dialog"
+			 * This function supports a variable number of arguments, 
+			 * meaning that you can pass as many key,value as you need to set all html attributes.
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putStartTag = function(tagName) {
+				if(!tagName) throw wigiiNcd.createServiceException('putStartTag takes a non null tagName', wigiiNcd.errorCodes.INVALID_ARGUMENT);
+				self.buffer += '<'+tagName;
+				if(arguments.length>1) {
+					var i = 1;
+					var key,value;
+					while(i<arguments.length) {
+						key = arguments[i];
+						if(!key) throw wigiiNcd.createServiceException('html attribute name cannot be null', wigiiNcd.errorCodes.INVALID_ARGUMENT);
+						i++;
+						if(i<arguments.length) {
+							value = arguments[i];
+							i++;
+						}
+						else value = '';
+						self.buffer += ' '+key+'="'+value+'"';
+					}
+				}
+				self.buffer += '>';
+				return self;
+			};
+			/**
+			 * Creates an html close tag
+			 * @param String tagName the name of the html tag to close, for example "div" or "p"
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putEndTag = function(tagName) {
+				if(!tagName) throw wigiiNcd.createServiceException('putEndTag takes a non null tagName', wigiiNcd.errorCodes.INVALID_ARGUMENT);
+				self.buffer += '</'+tagName+'>';
+				return self;
+			};
+			/**
+			 * Creates an html document header 
+			 * @return HtmlBuilder for chaining
+			 */
+			self.putHtmlHeader = function() {
+				self.buffer += '<!DOCTYPE html>';
+				return self;
+			};
+			/**
+			 * Puts some JS code into the buffer waiting for execution
+			 * @param Function|String actions the JS code to be executed. (a function with no arguments or a valid js code string).
+			 * @returns HtmlBuilder for chaining
+			 */
+			self.putJsCode = function(actions) {
+				var actionType = $.type(actions);
+				if(actionType==='function') {
+					self.jsBuffer.push(actions);
+				}
+				else if(actionType==='string') {
+					self.jsBuffer.push(function(){eval(actions);});
+				}
+				return self;
+			};
+			/**
+			 * Adds the content of the given HtmlBuilder to the current HtmlBuilder
+			 * @param HtmlBuilder htmlBuilder the HtmlBuilder instance from which to get the html string and waiting js code.
+			 */
+			self.putHtmlBuilder = function(htmlBuilder) {
+				if(htmlBuilder) {
+					// adds html
+					self.put(htmlBuilder.html());
+					// adds js code
+					for(var i=0;i<htmlBuilder.jsBuffer.length;i++) {
+						self.putJsCode(htmlBuilder.jsBuffer[i]);
+					}
+				}
+				return self;
+			};
+		};
+		
+		// Service providing
+		
+		/**
+		 * Creates a new StringStack instance
+		 */
+		wigiiNcd.createStringStackInstance = function() {
+			return new wigiiNcd.StringStack();
+		};
+		
+		/**
+		 * Creates a new HtmlEmitter object
+		 */
+		wigiiNcd.getHtmlEmitter = function(output) {
+			return new wigiiNcd.HtmlEmitter(output);
+		};
+		
+		/**
+		 * Creates an HtmlBuilder instance
+		 */
+		wigiiNcd.getHtmlBuilder = function() {			
+			return new wigiiNcd.HtmlBuilder();
+		};
+		
+		// Functions
+		
+		/**
+		 * throws a ServiceException::NOT_IMPLEMENTED exception
+		 */
+		wigiiNcd.throwNotImplemented = function() {
+			throw new wigiiNcd.ServiceException("not implemented", wigiiNcd.errorCodes.NOT_IMPLEMENTED);
+		};
+		/**
+		 * throws a ServiceException 
+		 */
+		wigiiNcd.createServiceException = function(message,code,previous) {
+			return new wigiiNcd.ServiceException(message, code, previous);
+		};
+		
 		/**
 		 * @return String returns the Wigii NCD version number
 		 */
-		wigiiNcd.version = function() {return "1.0";}
+		wigiiNcd.version = function() {return "1.0";};
 	},	
 	// Default WigiiNCD instance
 	wigiiNcdInstance = new WigiiNcd(),
@@ -49,5 +606,5 @@
 		return wigiiNcd;
 	};
 	// Bootstrap
-	window.wigiiNcd = wigiiNcdFacade;
+	if(!window.wigiiNcd || window.wigiiNcd().version() < wigiiNcdFacade().version()) window.wigiiNcd = wigiiNcdFacade;
  })(window, jQuery);
