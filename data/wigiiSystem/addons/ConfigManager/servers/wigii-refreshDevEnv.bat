ea49ae47079862dff61a945623f5ec9616ca015b
Camille Weber <cwebedev@users.noreply.github.com>
2019-05-02 16:53:20 +0200

    addons/ConfigManager: wigii-refreshDevEnv.bat: imports impl files + www files from live server when refreshing dev env.

--- data/wigiiSystem/addons/ConfigManager/servers/wigii-refreshDevEnv.bat
+++ data/wigiiSystem/addons/ConfigManager/servers/wigii-refreshDevEnv.bat
@@ -29,6 +29,8 @@ REM
 set WIGII_CLIENT=%1
 set USAGE=%0 wigii_client
 set RETURNVALUE=0
+SET PREVIOUS_PATH=%CD%
+cd %~dp0
 echo Changes code page to UTF-8
 chcp 65001
 
@@ -39,6 +41,8 @@ if "%WIGII_ADMIN_HOME%"=="" (echo Wigii ERROR: WIGII_ADMIN_HOME is not defined. 
 IF %WIGII_ADMIN_HOME:~-1%==\ SET WIGII_ADMIN_HOME=%WIGII_ADMIN_HOME:~0,-1%
 if "%WIGII_ENV%"=="" (echo Wigii ERROR: WIGII_ENV is not defined. Call %~nx0 from USER-adminConsole.bat & set RETURNVALUE=1009 & goto end)
 IF %WIGII_ENV:~-1%==\ SET WIGII_ENV=%WIGII_ENV:~0,-1%
+set WIGII_WWW=%WIGII_ENV%\www
+if not exist %WIGII_WWW% (echo Wigii ERROR: www folder has not been found & set RETURNVALUE=404 & goto end)
 
 rem checks installation of mysql tools
 if "%WIGII_MYSQL_ENV%"=="" (echo Wigii ERROR: WIGII_MYSQL_ENV is not defined. Call %~nx0 from USER-adminConsole.bat & set RETURNVALUE=1009 & goto end)
@@ -46,15 +50,30 @@ set MYSQL=%WIGII_MYSQL_ENV%\bin\mysql.exe
 if not exist %MYSQL% (echo Wigii ERROR: %MYSQL% does not exist & set RETURNVALUE=404 & goto end)
 if "%WIGII_MYSQL_ROOTPWD%"=="" (echo Wigii MySql root password is not set. Assumes empty string. If not, please set WIGII_MYSQL_ROOTPWD environment variable.)
 
+rem checks installation of WinSCP
+if exist "%ProgramFiles%\WinSCP\WinSCP.exe" (set WINSCP="%ProgramFiles%\WinSCP\WinSCP.exe") else (
+if exist "%ProgramFiles(x86)%\WinSCP\WinSCP.exe" (set WINSCP="%ProgramFiles(x86)%\WinSCP\WinSCP.exe") else (
+	echo Wigii ERROR: WinScp is not installed & set RETURNVALUE=404 & goto end
+))
+rem prepares Wigii server connexion string
+set WIGII_CONNEXION=open ftps://%WIGII_USER%:%WIGII_PWD%@%WIGII_SERVER% -explicit -certificate="%WIGII_CERTIFICATE%"
+set WINSCP_CMD=%WINSCP% /log=winscp.log /loglevel=0 /logsize=5*2M /command
+
 :getClientProdConfig
 echo Get latest production configuration files for %WIGII_CLIENT%
-call %WIGII_ADMIN_HOME%\configs\%WIGII_HOST%-versionProdConfig.bat %WIGII_CLIENT%
+rem uses start /B /W cmd /c ... instead of call ... to make sure environment variables are kept on completion.
+start /B /W "%WIGII_HOST%-versionProdConfig.bat" cmd /c %WIGII_ADMIN_HOME%\configs\%WIGII_HOST%-versionProdConfig.bat %WIGII_CLIENT%
 if %ERRORLEVEL% neq 0 (RETURNVALUE=%ERRORLEVEL% & goto end)
 
 :getClientProdDb
 echo Get latest production database for %WIGII_CLIENT%
+rem backups WIGII_CONNEXION and WINSCP_CMD
+set WIGII_CONNEXION_BACKUP=%WIGII_CONNEXION%
+set WINSCP_CMD_BACKUP=%WINSCP_CMD%
 call %WIGII_ADMIN_HOME%\db-dump\%WIGII_HOST%-getDb.bat %WIGII_CLIENT%
 if %ERRORLEVEL% neq 0 (RETURNVALUE=%ERRORLEVEL% & goto end)
+set WIGII_CONNEXION=%WIGII_CONNEXION_BACKUP%
+set WINSCP_CMD=%WINSCP_CMD_BACKUP%
 set WIGII_DB_DUMP=%WIGII_DB%
 if "%WIGII_DB_DUMP%"=="" (echo Wigii ERROR: No db dump downloaded from production for %WIGII_CLIENT% & set RETURNVALUE=404 & goto end)
 
@@ -68,6 +87,8 @@ mkdir %WIGII_TARGET%
 call %~dp0%WIGII_HOST%-setup.bat %WIGII_TARGET%
 if "%WIGII_TARGET_WEB%"=="" (echo Wigii ERROR: WIGII_TARGET_WEB is not defined. & set RETURNVALUE=1009 & goto end)
 if "%WIGII_TARGET_ENV%"=="" (echo Wigii ERROR: WIGII_TARGET_ENV is not defined. & set RETURNVALUE=1009 & goto end)
+if "%WIGII_HOST_WEB%"=="" (echo Wigii ERROR: WIGII_HOST_WEB is not defined. & set RETURNVALUE=1009 & goto end)
+if "%WIGII_HOST_ENV%"=="" (echo Wigii ERROR: WIGII_HOST_ENV is not defined. & set RETURNVALUE=1009 & goto end)
 mkdir %WIGII_TARGET_WEB%
 mkdir %WIGII_TARGET_ENV%
 
@@ -76,6 +97,26 @@ mkdir %WIGII_TARGET_ENV%\data\wigiiSystem\configs\%WIGII_CLIENT%
 xcopy %WIGII_ADMIN_HOME%\configs\%WIGII_CLIENT%-prod\* %WIGII_TARGET_ENV%\data\wigiiSystem\configs\%WIGII_CLIENT% /e /s
 ren %WIGII_TARGET_ENV%\data\wigiiSystem\configs\%WIGII_CLIENT%\start.php start-prod.php
 ren %WIGII_TARGET_ENV%\data\wigiiSystem\configs\%WIGII_CLIENT%\start_cli.php start_cli-prod.php
+
+:getClientProdImplFiles
+echo Downloads api\impl\%WIGII_CLIENT% files
+mkdir %WIGII_TARGET_ENV%\data\wigiiSystem\core\api\impl\%WIGII_CLIENT%
+%WINSCP_CMD% ^
+ "%WIGII_CONNEXION%" ^
+ "lcd %WIGII_TARGET_ENV%\data\wigiiSystem\core\api\impl\%WIGII_CLIENT%" ^
+ "get -transfer=binary %WIGII_HOST_ENV%/data/wigiiSystem/core/api/impl/%WIGII_CLIENT%/*.?* .\" ^
+  "close" ^
+ "exit"
+if %ERRORLEVEL% neq 0 goto winScpError
+echo Downloads www\%WIGII_CLIENT% files
+mkdir %WIGII_TARGET_WEB%\%WIGII_CLIENT%
+%WINSCP_CMD% ^
+ "%WIGII_CONNEXION%" ^
+ "lcd %WIGII_TARGET_WEB%\%WIGII_CLIENT%" ^
+ "get -transfer=binary %WIGII_HOST_WEB%/%WIGII_CLIENT%/*.?* .\" ^
+  "close" ^
+ "exit"
+if %ERRORLEVEL% neq 0 goto winScpError
 
 :importsDb
 rem checks that db dump exists before imports
@@ -106,15 +147,42 @@ del /Q %WIGII_ENV%\data\wigiiSystem\configs\%WIGII_CLIENT%\start_cli-prod.php
 ren %WIGII_TARGET_ENV%\data\wigiiSystem\configs\%WIGII_CLIENT%\start.php start-dev.php
 ren %WIGII_TARGET_ENV%\data\wigiiSystem\configs\%WIGII_CLIENT%\start_cli.php start_cli-dev.php
 
+:refreshDevImpl
+echo Refreshes dev api\impl\%WIGII_CLIENT%
+copy /Y %WIGII_TARGET_ENV%\data\wigiiSystem\core\api\impl\%WIGII_CLIENT%\*.* %WIGII_ENV%\data\wigiiSystem\core\api\impl\%WIGII_CLIENT%
+echo Refreshes dev www\%WIGII_CLIENT%
+copy /Y %WIGII_TARGET_WEB%\%WIGII_CLIENT%\*.* %WIGII_WWW%\%WIGII_CLIENT%
+
 echo Done. Dev env %WIGII_CLIENT% refreshed from production.
 goto end
 :mySqlError
 set RETURNVALUE=2501
 echo Erreur de MySql
 goto end
+:winScpError
+set RETURNVALUE=10303
+echo Erreur de communication WinScp
+goto end
 :end
 REM clears all variables and exits with return value
 set MYSQL_CMD=
 if not "%MYSQL_CMDFILE%"=="" del /Q %MYSQL_CMDFILE%
 SET MYSQL_CMDFILE=
+set WIGII_SERVER=
+set WIGII_CERTIFICATE=
+set WIGII_USER=
+set WIGII_PWD=
+set WINSCP=
+set WINSCP_CMD=
+set WINSCP_CMD_BACKUP=
+set WIGII_CONNEXION=
+set WIGII_CONNEXION_BACKUP=
+set WIGII_HOST=
+set WIGII_HOST_WEB=
+set WIGII_HOST_ENV=
+set WIGII_WWW=
+set WIGII_TARGET=
+set WIGII_TARGET_WEB=
+set WIGII_TARGET_ENV=
+cd %PREVIOUS_PATH%
 exit /b %RETURNVALUE%
